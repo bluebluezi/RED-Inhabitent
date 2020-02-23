9384e00cfc9af6c9fbf25e9f3381c736597c224f
bluebluezi <ng.johnnyzi@gmail.com>
2020-02-23 18:53:45 -0500

    feature: added logo and search favicon for the header, added footer in php file

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -1,4 +1,7 @@
 <?php wp_footer();?>
 
 </body>
+<footer>
+    <p>COPYRIGHT &copy 2020 INHABITENT</p>
+</footer>
 </html>
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -7,9 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
 <body <?php body_class();?>>
-<h1><?php bloginfo('name');?></h1>
-<nav class ="main-menu">
+<header>
+<!-- <h1><?php bloginfo('name');?></h1> -->
+
+<!-- chooses between the logo images using if statement -->
+<img src="assets/images/logo/inhabitent-logo-tent.svg">
+<nav class ="main-menu-wrapper">
     <?php wp_nav_menu(array(
         'theme_location' => 'primary'
     ));?>
 </nav>
+<i class="fas fa-search fa-2x"></i>
+</header>
