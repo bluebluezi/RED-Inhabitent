c2cb51d15af7680997fccdac1d7e42e5b9d91882
bluebluezi <ng.johnnyzi@gmail.com>
2020-02-27 11:18:40 -0500

    style: applied grid to shop categories that scales responsively

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -2,9 +2,10 @@
 
 <?php if( have_posts() ) :
 
-//The WordPress Loop: loads post content -->
+//The WordPress Loop: loads post content 
     while( have_posts() ) :
         the_post(); ?>
+
     <div class="home-hero-banner">
         <?php the_post_thumbnail('full');?>
     <img 
@@ -13,7 +14,7 @@
     </div>
     <?php the_content(); ?>
     
-
+    <!-- Loop ends -->
     <?php endwhile;?>
 
     <?php the_posts_navigation();?>
@@ -22,13 +23,53 @@
         <p>No posts found</p>
 <?php endif;?>
 
+<?php 
+$terms = get_terms(array(
+    'taxonomy' => 'product-type',
+    'hide_empty' => false
+));
+?>
+
+<?php
+echo "<p>";
+echo $term->name;
+echo "</p>";?>
+
+<section class="shop-categories-wrapper">
+
+<?php
+foreach($terms as $term) : ?>
+    <div class="shop-categories">
+
+    <?php
+    $file_name = $term->name . '.svg';
+    // echo "<p>";
+    // echo $term->name;
+    // echo "</p>";?>
+
+    <img src='<?php echo get_template_directory_uri() . "/assets/images/product-type-icons/$file_name"?>'>
+
+    </div>
+<?php endforeach;?>
+
+</section>
 
 
+<!-- Custom Post Loop Starts -->
+<?php
+   $args = array( 
+       'post_type' => 'post', 
+       'order' => 'ASC',
+       'numberposts' => 3
+    );
+   $product_posts = get_posts( $args ); // returns an array of posts
 
+?>
+<?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+   <?php the_title() ?>
+   <?php the_post_thumbnail() ?>
+  
+<?php endforeach; wp_reset_postdata(); ?>
 
-<!-- custom loop start -->
-
-
-<!-- custom Loop End -->
     
 <?php get_footer();?>
