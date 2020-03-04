c7b19538f0f7cbadce5aa2bdab225c6a70e8e9a6
bluebluezi <ng.johnnyzi@gmail.com>
2020-03-04 15:10:19 -0500

    style: populated blog index page with appropriate items

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -1,23 +1,53 @@
 <?php get_header(); ?>
 
 <?php if( have_posts() ) :
-print_r($post);
 //The WordPress Loop: loads post content 
-    while( have_posts() ) :
-        the_post(); ?>
-    
-    <h2><?php the_title(); ?></h2>
-    <h3><?php the_permalink();?></h3>
-    <?php the_content(); ?>
-    
-    <!-- Loop ends -->
-    <?php endwhile;?>
+?>
+<div class="blog-post-index-wrapper">
+    <section class="blog-post-index-content">
+    <?php while( have_posts() ) :
+            the_post(); 
+            print_r($post) ?>
+    <figure class="blog-post">
+        <!-- <div class="post-image-wrapper">
+            <?php echo the_post_thumbnail('full');?>
+        </div> -->
+        <div class="post-title-wrapper">
+            <h2><?php the_title(); ?></h2>
+        </div>
+        <div class ="post-info-wrapper">
+        <?php 
+                $postDateRaw = $post->post_date;
+                $postDate= date('t F Y', strtotime($postDateRaw));
+                $commentCount = $post->comment_count;
+                $author = $post->post_author;
+                echo "<p>";
+                echo $postDate . " / " . $commentCount . " Comments" . " " .
+                     get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name');
+                echo "</p>";
+                
+            ?>
+        </div>
+        <figcaption>
+        <?php echo wp_trim_words(get_the_content(), 20, '[...]'); ?>
+        <figcaption>
+    </figure>
+        <!-- Loop ends -->
+        <?php endwhile;?>
+    </section>
+    <?php get_sidebar();?>
+</div>
 
-    <?php the_posts_navigation();?>
 
 <?php else : ?>
-        <p>No posts found</p>
+    <div class="blogpost-index-wrapper">
+        <section class ="blogpost-index-content">
+            <p>No posts found</p>
+        </section>
+        <?php get_sidebar();?>
+    </div>
+        
 <?php endif;?>
 
-    
+
 <?php get_footer();?>
