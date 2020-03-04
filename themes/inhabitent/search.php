917bc52a40126c1ad20a1f51d4093d26d0210642
bluebluezi <ng.johnnyzi@gmail.com>
2020-03-04 10:25:08 -0500

    feat: added and populated default search results page

---
+++ themes/inhabitent/search.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<p>You searched for " <?php echo esc_html(get_search_query(false));?> ". Here are the results:"</p>
+<?php if( have_posts() ) :
+//The WordPress Loop: loads post content 
+    while( have_posts() ) :
+        the_post(); ?>
+    
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+    
+    <!-- Loop ends -->
+    <?php endwhile;?>
+
+    <?php the_posts_navigation();?>
+
+<?php else : ?>
+        <p>No posts found</p>
+<?php endif;?>
+
+    
+<?php get_footer();?>
