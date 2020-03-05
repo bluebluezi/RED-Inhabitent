ca390a6e43c5e2753c3218b16a9e8babf001167c
bluebluezi <ng.johnnyzi@gmail.com>
2020-03-05 18:46:24 -0500

    style: styled buttons

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -5,33 +5,36 @@
 ?>
 <div class="blog-post-index-wrapper">
     <section class="blog-post-index-content">
-    <?php while( have_posts() ) :
-            the_post(); 
-            print_r($post) ?>
-    <figure class="blog-post">
-        <!-- <div class="post-image-wrapper">
-            <?php echo the_post_thumbnail('full');?>
-        </div> -->
-        <div class="post-title-wrapper">
-            <h2><?php the_title(); ?></h2>
-        </div>
-        <div class ="post-info-wrapper">
-        <?php 
-                $postDateRaw = $post->post_date;
-                $postDate= date('t F Y', strtotime($postDateRaw));
-                $commentCount = $post->comment_count;
-                $author = $post->post_author;
-                echo "<p>";
-                echo $postDate . " / " . $commentCount . " Comments" . " " .
-                     get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name');
-                echo "</p>";
-                
-            ?>
-        </div>
-        <figcaption>
-        <?php echo wp_trim_words(get_the_content(), 20, '[...]'); ?>
-        <figcaption>
-    </figure>
+        <?php while( have_posts() ) : the_post();?>
+        <figure class="blog-post">
+            <div class="post-image-wrapper">
+                <?php echo the_post_thumbnail();?>
+                <div class="post-title-wrapper">
+                    <h2><?php the_title(); ?></h2>
+                </div>
+                <div class ="post-info-wrapper">
+                <?php 
+                    $postDateRaw = $post->post_date;
+                    $postDate= date('t F Y', strtotime($postDateRaw));
+                    $commentCount = $post->comment_count;
+                    $author = $post->post_author;
+                    echo "<p>";
+                    echo $postDate . " / " . $commentCount . " Comments" . " " .
+                        get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name');
+                    echo "</p>";    
+                ?>
+                </div>
+            </div>
+
+            <figcaption class="blog-post-content">
+                <?php echo wp_trim_words(get_the_content(), 50, '[...]'); ?>
+            </figcaption>
+
+            <a class="post-button" href="<?php the_permalink();?>">
+            Read More →
+            </a>
+            <!-- button here -->
+        </figure>
         <!-- Loop ends -->
         <?php endwhile;?>
     </section>
