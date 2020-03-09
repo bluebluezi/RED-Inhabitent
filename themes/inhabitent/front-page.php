315f8f96f1a1f6db1b3991ef5556d76565b6431b
bluebluezi <ng.johnnyzi@gmail.com>
2020-03-09 03:40:52 -0400

    style: renamed classes to match section headings and added buttons to cells in all sections

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -34,12 +34,14 @@
     <!-- <?php
     print_r($terms);?> //this is used to print out $terms -->
 
-<section class="shop-categories">
-    <div class="shop-categories-container">
-        <h2>SHOP STUFF</h2>
+<section class="product-categories">
+    <h2>SHOP STUFF</h2>
+    <div class="product-categories-container">
+        
 
         <?php foreach($terms as $term) : ?>
-            <figure class="shop-categories-cell">
+            
+            <figure class="product-categories-cell">
 
                 <?php
                     $file_name = $term->name . '.svg';
@@ -52,6 +54,14 @@
                     echo $descriptions;
                     echo "</p>";
                 ?>
+
+                <a class="product-categories-button" href="<?php the_permalink();?>">
+                    
+                    <?php $name = $term->name;
+                        echo $name . " stuff";
+                    ?>
+
+                </a>
                 <!-- add button here -->
             </figure>
         <?php endforeach;?>
@@ -59,9 +69,10 @@
 </section>
 
 
-<section class="journal">
-    <div class="journal-container">
-
+<section class="inhabitent-journal">
+    <h2>Inhabitent Journal</h2>
+    <div class="inhabitent-journal-container">
+        
         <?php
         $args = array( 
             'post_type' => 'post', 
@@ -72,21 +83,27 @@
 
         <?php foreach ( $product_posts as $post ) : setup_postdata( $post );?>
         
-            <figure class="journal-cell">
+            <figure class="inhabitent-journal-cell">
 
                 <?php the_post_thumbnail() ?> 
 
-                <?php 
-                    $postDateRaw = $post->post_date;
-                    $postDate= date('t F Y', strtotime($postDateRaw));
-                    $commentCount = $post->comment_count;
-                    echo "<p>";
-                    echo $postDate . " / " . $commentCount . " Comments";
-                    echo "</p>";
-                ?>    
+                <figcaption class="inhabitent-journal-cell-text">
 
-                <h3><?php the_title() ?></h3>
-                <!-- add button here -->
+                    <?php 
+                        $postDateRaw = $post->post_date;
+                        $postDate= date('t F Y', strtotime($postDateRaw));
+                        $commentCount = $post->comment_count;
+                        echo "<p>";
+                        echo $postDate . " / " . $commentCount . " Comments";
+                        echo "</p>";
+                    ?>    
+
+                    <h3><?php the_title() ?></h3>
+                </figcaption>
+                <a class="inhabitent-journal-button" href="<?php the_permalink();?>">
+                    Read Entry
+                </a>
+
             </figure>
     
         <?php endforeach; wp_reset_postdata(); ?>
@@ -114,13 +131,13 @@
                 <h3><?php the_title() ?></h3>
                 <!-- add button here -->
                 <a class="custom-posttype-button" href="<?php the_permalink();?>">
-                    Read More →
+                    Read More
                 </a>
             </figure>
 
         <?php endforeach; wp_reset_postdata(); ?>
 
-        <a class="custom-posttype-button" href="<?php the_permalink();?>">
+        <a class="more-adventures-button" href="<?php the_permalink();?>">
             More Adventures
         </a>
     </div>
